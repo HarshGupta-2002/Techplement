1514112b0de763959456505b4a485f57dd1d9448
harsh gupta <harsh2002.gupta@gmail.com>
2024-08-24 23:53:55 +0530

    hoster server api calls

--- week1-tasks/client/src/components/Login.js
+++ week1-tasks/client/src/components/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/users/login', {
+            const response = await axios.post('${process.env.REACT_APP_API_URL}/api/users/login', {
                 username,
                 password,
             });
